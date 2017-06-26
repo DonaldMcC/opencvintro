673dc0c93503216209730b030037730110ebc0ba
Donald McClymont <donald_mcclymont@hotmail.com>
2017-06-26 23:14:34 +0100

    initial test

--- displaycamera.py
+++ displaycamera.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 
 clicked = False
@@ -7,14 +8,19 @@ def onMouse(event, x, y, flags, param):
         clicked = True
 
 cameraCapture = cv2.VideoCapture(0)
-cv2.namedWindow('MyWindow')
+fps = 30 # an assumption
+size = (int(cameraCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cameraCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+cv2.namedWindow('MyWindow', cv2.WINDOW_NORMAL)
 cv2.setMouseCallback('MyWindow', onMouse)
 
 print 'Showing camera feed. Click window or press any key to stop.'
-success, frame = cameraCapture.read()
-while success and not clicked:
-    cv2.imshow('MyWindow', frame)
-    success, frame = cameraCapture.read()
 
-cv2.destroyWindow('MyWindow')
+while not clicked:
+    success, frame = cameraCapture.read()
+    #gray = cv2.cvtColor(frame, cv2)
+    cv2.imshow('MyWindow', frame)
+    cv2.waitKey(25)
+
 cameraCapture.release()
+cv2.destroyAllWindows()
