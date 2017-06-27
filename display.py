5a66d4730a786cf8a64943b4d8b76c949e3addde
Donald McClymont <donald_mcclymont@hotmail.com>
2017-06-27 22:53:43 +0100

    fixed camera capture to work on windows

--- display.py
+++ display.py
@@ -1,5 +1,4 @@
 import cv2
-
 clicked = False
 
 
@@ -8,8 +7,12 @@ def onMouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONUP:
         clicked = True
 
-videoCapture = cv2.VideoCapture('IMG_0464.MOV')
-fps = videoCapture.get(cv2.CAP_PROP_FPS)
+#videoCapture = cv2.VideoCapture('MyOutputVid.avi')
+videoCapture = cv2.VideoCapture('kite1.avi')
+print videoCapture.grab() 
+
+#fps = videoCapture.get(cv2.CAP_PROP_FPS)
+fps = 25
 size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
         int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
@@ -20,7 +23,10 @@ print 'Showing camera feed. Click window or press any key to stop.'
 success, frame = videoCapture.read()
 while success and not clicked:
     cv2.imshow('MyWindow', frame)
+    cv2.waitKey(100)
     success, frame = videoCapture.read()
+
+    
 
 cv2.destroyWindow('MyWindow')
 videoCapture.release()
